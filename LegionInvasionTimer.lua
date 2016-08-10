a11a0efe0989f5f5a8cacdfbe50c01b81a40c515
funkydude <funkydude@users.noreply.github.com>
2016-08-10 18:50:42 +0100

    Cosmetics

--- LegionInvasionTimer.lua
+++ LegionInvasionTimer.lua
@@ -10,11 +10,14 @@ frame:SetScript("OnEvent", function()
 	for i=1, 300 do
 		local name, timeLeftMinutes, rewardQuestID = GetInvasionInfoByMapAreaID(i)
 		if timeLeftMinutes then
-			local bar = candy:New(media:Fetch("statusbar", "BantoBar"), 200, 10)
+			local bar = candy:New(media:Fetch("statusbar", "BantoBar"), 200, 30)
 			bar:SetLabel("Invasion")
+			bar.candyBarLabel:SetJustifyH("LEFT")
 			bar:SetDuration(timeLeftMinutes * 60)
+			bar:SetIcon(236292) -- Interface\\Icons\\Ability_Warlock_DemonicEmpowerment
 			bar:SetPoint("CENTER", UIParent, "CENTER")
 			bar:Start()
+			break
 		end
 	end
 end)
