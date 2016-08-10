927fb967f9010aa28a8e9cc18cae81421bf6831b
funkydude <funkydude@users.noreply.github.com>
2016-08-10 21:30:04 +0100

    The very first login doesn't seem to have GetInvasionInfoByMapAreaID data fast enough, delay it a little.

--- LegionInvasionTimer.lua
+++ LegionInvasionTimer.lua
@@ -1,5 +1,7 @@
 
 local name = ...
+local candy = LibStub("LibCandyBar-3.0")
+local media = LibStub("LibSharedMedia-3.0")
 
 local frame = CreateFrame("Frame", "LegionInvasionTimer", UIParent)
 frame:SetPoint("CENTER", UIParent, "CENTER")
@@ -12,18 +14,12 @@ frame:SetClampedToScreen(true)
 frame:SetScript("OnDragStart", function(f) f:StartMoving() end)
 frame:SetScript("OnDragStop", function(f) f:StopMovingOrSizing() end)
 frame:RegisterEvent("PLAYER_LOGIN")
-
 local bg = frame:CreateTexture(nil, "PARENT")
 bg:SetAllPoints(frame)
 bg:SetColorTexture(0, 1, 0, 0.3)
-
 local header = frame:CreateFontString("TargetPercentText", "OVERLAY", "TextStatusBarText")
 header:SetAllPoints(frame)
 header:SetText(name)
-
--- LIBRARIES
-local candy = LibStub("LibCandyBar-3.0")
-local media = LibStub("LibSharedMedia-3.0")
 
 local function startBar(timeLeft)
 	local bar = candy:New(media:Fetch("statusbar", "BantoBar"), 200, 30)
@@ -35,27 +31,34 @@ local function startBar(timeLeft)
 	bar:Start()
 end
 
-frame:SetScript("OnEvent", function()
+local function runOnLogin()
 	local found = false
 
-	for i=1, 300 do
+	for i = 1, 300 do
 		local name, timeLeftMinutes, rewardQuestID = GetInvasionInfoByMapAreaID(i)
 		if timeLeftMinutes and timeLeftMinutes > 0 then
 			found = true
-			invasionTimerDB = {GetTime(), timeLeftMinutes}
+			legionInvasionTimerDB = {GetTime(), timeLeftMinutes}
 
 			startBar(timeLeftMinutes * 60)
 			break
 		end
 	end
 
-	if not found and invasionTimerDB then
-		local t, rem = invasionTimerDB[1], invasionTimerDB[2]
+	if not found and legionInvasionTimerDB then
+		local t, rem = legionInvasionTimerDB[1], legionInvasionTimerDB[2]
 		if t and rem then
+			found = true
 			local deduct = (GetTime() - t) / 60
 			local timeLeftMinutes = rem - deduct
 			startBar(timeLeftMinutes * 60)
 		end
 	end
-end)
+
+	if not found then
+		C_Timer.After(7, runOnLogin) -- The very first login doesn't have GetInvasionInfoByMapAreaID data fast enough, delay it
+	end
+end
+
+frame:SetScript("OnEvent", runOnLogin)
 
