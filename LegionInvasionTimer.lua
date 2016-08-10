08176b0ec5efe8956aae0832fc6d2e0e680be08d
funkydude <funkydude@users.noreply.github.com>
2016-08-10 20:15:50 +0100

    Add timer saving to properly restore the bar when you've completed all events.

--- LegionInvasionTimer.lua
+++ LegionInvasionTimer.lua
@@ -25,18 +25,37 @@ header:SetText(name)
 local candy = LibStub("LibCandyBar-3.0")
 local media = LibStub("LibSharedMedia-3.0")
 
+local function startBar(timeLeft)
+	local bar = candy:New(media:Fetch("statusbar", "BantoBar"), 200, 30)
+	bar:SetLabel("Invasion")
+	bar.candyBarLabel:SetJustifyH("LEFT")
+	bar:SetDuration(timeLeft)
+	bar:SetIcon(236292) -- Interface\\Icons\\Ability_Warlock_DemonicEmpowerment
+	bar:SetPoint("TOP", LegionInvasionTimer, "BOTTOM")
+	bar:Start()
+end
+
 frame:SetScript("OnEvent", function()
+	local found = false
+
 	for i=1, 300 do
 		local name, timeLeftMinutes, rewardQuestID = GetInvasionInfoByMapAreaID(i)
-		if timeLeftMinutes then
-			local bar = candy:New(media:Fetch("statusbar", "BantoBar"), 200, 30)
-			bar:SetLabel("Invasion")
-			bar.candyBarLabel:SetJustifyH("LEFT")
-			bar:SetDuration(timeLeftMinutes * 60)
-			bar:SetIcon(236292) -- Interface\\Icons\\Ability_Warlock_DemonicEmpowerment
-			bar:SetPoint("TOP", LegionInvasionTimer, "BOTTOM")
-			bar:Start()
+		if timeLeftMinutes and timeLeftMinutes > 0 then
+			found = true
+			invasionTimerDB = {GetTime(), timeLeftMinutes}
+
+			startBar(timeLeftMinutes * 60)
 			break
 		end
 	end
+
+	if not found and invasionTimerDB then
+		local t, rem = invasionTimerDB[1], invasionTimerDB[2]
+		if t and rem then
+			local deduct = (GetTime() - t) / 60
+			local timeLeftMinutes = rem - deduct
+			startBar(timeLeftMinutes * 60)
+		end
+	end
 end)
+
