81cdad50e9c1fa238aa438fb02470982e32524ed
funkydude <funkydude@users.noreply.github.com>
2016-08-10 19:25:22 +0100

    add a movable parent frame

--- LegionInvasionTimer.lua
+++ LegionInvasionTimer.lua
@@ -1,6 +1,25 @@
 
-local frame = CreateFrame("Frame")
+local name = ...
+
+local frame = CreateFrame("Frame", "LegionInvasionTimer", UIParent)
+frame:SetPoint("CENTER", UIParent, "CENTER")
+frame:SetWidth(180)
+frame:SetHeight(15)
+frame:SetMovable(true)
+frame:EnableMouse(true)
+frame:RegisterForDrag("LeftButton")
+frame:SetClampedToScreen(true)
+frame:SetScript("OnDragStart", function(f) f:StartMoving() end)
+frame:SetScript("OnDragStop", function(f) f:StopMovingOrSizing() end)
 frame:RegisterEvent("PLAYER_LOGIN")
+
+local bg = frame:CreateTexture(nil, "PARENT")
+bg:SetAllPoints(frame)
+bg:SetColorTexture(0, 1, 0, 0.3)
+
+local header = frame:CreateFontString("TargetPercentText", "OVERLAY", "TextStatusBarText")
+header:SetAllPoints(frame)
+header:SetText(name)
 
 -- LIBRARIES
 local candy = LibStub("LibCandyBar-3.0")
@@ -15,7 +34,7 @@ frame:SetScript("OnEvent", function()
 			bar.candyBarLabel:SetJustifyH("LEFT")
 			bar:SetDuration(timeLeftMinutes * 60)
 			bar:SetIcon(236292) -- Interface\\Icons\\Ability_Warlock_DemonicEmpowerment
-			bar:SetPoint("CENTER", UIParent, "CENTER")
+			bar:SetPoint("TOP", LegionInvasionTimer, "BOTTOM")
 			bar:Start()
 			break
 		end
