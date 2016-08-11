c65f0d917af22be5b93e04000456e70468cc0d1c
funkydude <funkydude@users.noreply.github.com>
2016-08-11 06:22:54 +0100

    Shuffle some code around, update bar colors when completing an event.

--- LegionInvasionTimer.lua
+++ LegionInvasionTimer.lua
@@ -1,9 +1,4 @@
 
-local weekday, month, day, year = CalendarGetDate()
-if month ~= 8 or year ~= 2016 then
-	return -- Good times come to an end
-end
-
 local name = ...
 local candy = LibStub("LibCandyBar-3.0")
 local media = LibStub("LibSharedMedia-3.0")
@@ -17,35 +12,14 @@ frame:SetMovable(true)
 frame:EnableMouse(true)
 frame:RegisterForDrag("LeftButton")
 frame:SetClampedToScreen(true)
-frame:SetScript("OnDragStart", function(f) f:StartMoving() end)
-frame:SetScript("OnDragStop", function(f) f:StopMovingOrSizing() end)
-frame:SetScript("OnMouseUp", function(f, btn)
-	if btn == "RightButton" then
-		LoadAddOn("LegionInvasionTimer_Options")
-		LibStub("AceConfigDialog-3.0"):Open(name)
-	end
-end)
-frame:SetScript("OnEnter", function(f)
-	GameTooltip:SetOwner(f, "ANCHOR_NONE")
-	GameTooltip:SetPoint("BOTTOM", f, "TOP")
-	GameTooltip:AddLine("|cffeda55fClick|r to drag and move.", 0.2, 1, 0.2, 1)
-	GameTooltip:AddLine("|cffeda55fRight-Click|r to open options.", 0.2, 1, 0.2, 1)
-	GameTooltip:Show()
-end)
-frame:SetScript("OnLeave", GameTooltip_Hide)
+frame:Hide()
 frame:RegisterEvent("PLAYER_LOGIN")
-local bg = frame:CreateTexture(nil, "PARENT")
-bg:SetAllPoints(frame)
-bg:SetColorTexture(0, 1, 0, 0.3)
-local header = frame:CreateFontString(nil, "OVERLAY", "TextStatusBarText")
-header:SetAllPoints(frame)
-header:SetText(name)
 
 local aboutToStopBar = false
 local function startBar(zone, timeLeft, rewardQuestID, first)
 	local bar
 	if first then
-		header:SetText(name) -- We may have changed the header after the event ended
+		frame.header:SetText(name) -- We may have changed the header after the event ended
 		if frame.bar1 then aboutToStopBar = true frame.bar1:Stop() aboutToStopBar = false end
 		frame.bar1 = candy:New(media:Fetch("statusbar", frame.optionsTbl.texture), frame.optionsTbl.width, frame.optionsTbl.height)
 		bar = frame.bar1
@@ -88,20 +62,22 @@ local function findTimer()
 	if first then
 		-- XXX Turn this into paused bars
 		count = count + 1
-		header:SetText("Searching ".. (count == 1 and "-" or count == 2 and "\\" or count == 3 and "|" or "/"))
+		frame.header:SetText("Searching  ".. (count == 1 and "-" or count == 2 and "\\" or count == 3 and "|" or "/"))
 		if count == 4 then count = 0 end
 		Timer(3, findTimer) -- Start hunting for the next event
 	end
 end
 
-candy.RegisterCallback(name, "LibCandyBar_Stop", function(_, bar)
-	if not aboutToStopBar and bar == frame.bar1 then
-		Timer(20, findTimer) -- Start hunting for the next event
-	end
-end)
+frame:SetScript("OnEvent", function(f)
+	f:UnregisterEvent("PLAYER_LOGIN")
 
-frame:SetScript("OnEvent", function()
-	frame.optionsTbl = { -- XXX not saving for now
+	local weekday, month, day, year = CalendarGetDate()
+	if month ~= 8 or year ~= 2016 then
+		f:SetScript("OnEvent", nil)
+		return -- Good times come to an end
+	end
+
+	f.optionsTbl = { -- XXX not saving for now
 		fontSize = 10,
 		texture = "BantoBar",
 		outline = "NONE",
@@ -110,6 +86,45 @@ frame:SetScript("OnEvent", function()
 		height = 30,
 		icon = true,
 	}
+
+	f:Show()
+	f:SetScript("OnDragStart", function(f) f:StartMoving() end)
+	f:SetScript("OnDragStop", function(f) f:StopMovingOrSizing() end)
+	f:SetScript("OnMouseUp", function(f, btn)
+		if btn == "RightButton" then
+			LoadAddOn("LegionInvasionTimer_Options")
+			LibStub("AceConfigDialog-3.0"):Open(name)
+		end
+	end)
+	f:SetScript("OnEnter", function(f)
+		GameTooltip:SetOwner(f, "ANCHOR_NONE")
+		GameTooltip:SetPoint("BOTTOM", f, "TOP")
+		GameTooltip:AddLine("|cffeda55fClick|r to drag and move.", 0.2, 1, 0.2, 1)
+		GameTooltip:AddLine("|cffeda55fRight-Click|r to open options.", 0.2, 1, 0.2, 1)
+		GameTooltip:Show()
+	end)
+	f:SetScript("OnLeave", GameTooltip_Hide)
+	local bg = f:CreateTexture(nil, "PARENT")
+	bg:SetAllPoints(f)
+	bg:SetColorTexture(0, 1, 0, 0.3)
+	local header = f:CreateFontString(nil, "OVERLAY", "TextStatusBarText")
+	header:SetAllPoints(f)
+	header:SetText(name)
+	f.header = header
+
+	candy.RegisterCallback(name, "LibCandyBar_Stop", function(_, bar)
+		if not aboutToStopBar and bar == frame.bar1 then
+			Timer(20, findTimer) -- Start hunting for the next event
+		end
+	end)
+
 	findTimer()
+	f:RegisterEvent("SCENARIO_COMPLETED")
+	f:SetScript("OnEvent", function()
+		local _,_,_,_,_,_,_,_,_,scenarioType = C_Scenario.GetInfo()
+		if scenarioType == 4 then -- LE_SCENARIO_TYPE_LEGION_INVASION = 4
+			Timer(8, findTimer) -- Update bar color
+		end
+	end)
 end)
 
