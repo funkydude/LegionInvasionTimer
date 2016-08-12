b59562149ac1e36153c9ca35f22878ed2a730e77
funkydude <funkydude@users.noreply.github.com>
2016-08-12 23:20:54 +0100

    Add more boss abilities

--- Bosses.lua
+++ Bosses.lua
@@ -2,6 +2,7 @@
 local f = CreateFrame("Frame")
 local name, mod = ...
 local colorTbl = {r=1,g=1,b=1}
+local myID = UnitGUID("player")
 f:SetScript("OnEvent", function(frame, event, ...)
 	mod[event](mod, ...)
 end)
@@ -26,6 +27,7 @@ function mod:SCENARIO_UPDATE()
 	for i = 3, 8 do
 		local _,_, rewardQuestIDInv = GetInvasionInfo(i)
 		if rewardQuestID == rewardQuestIDInv and currentStage == 4 then
+			myID = UnitGUID("player")
 			f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED") -- Boss is coming up, register
 		end
 	end
@@ -43,6 +45,7 @@ do
 			[217134] = "|T".. GetSpellTexture(217134) ..texString.. GetSpellInfo(217134).. " (FRONTAL CLEAVE)", -- Vampiric Cleave 33s
 			[217040] = "|T".. GetSpellTexture(217040) ..texString.. GetSpellInfo(217040).. " (THREAT WIPE - SPAWN ADD)", -- Shadow Illusion 35s
 			[216916] = "|T".. GetSpellTexture(216916) ..texString.. GetSpellInfo(216916).. " (FRONTAL CONE DMG)", -- Waves of Dread 35s
+			[219469] = "|T".. GetSpellTexture(219469) ..texString.. GetSpellInfo(219469).. " (KILL THEM FAST)", -- Summon Explosive Orbs 42s
 		},
 		SPELL_AURA_REMOVED = {
 			[219112] = "|T".. GetSpellTexture(219112) ..texString.. GetSpellInfo(219112).. " (FINISHED)", -- Eye of Darkness 45s
@@ -58,14 +61,19 @@ do
 
 		if event == "SPELL_AURA_APPLIED" then
 			if spellId == 219176 then -- Secrete Shadows 31-41s
-				 -- 10 sec debuff on tank
+				-- 10 sec debuff on tank
 				local msg = "|T".. GetSpellTexture(spellId) ..texString.. spellName.. ": ".. gsub(destName, "%-.+", "*") .."(TANK SWAP)"
 				print("|cFF33FF99LegionInvasionTimer|r:", msg)
 				RaidNotice_AddMessage(RaidBossEmoteFrame, msg, colorTbl, 4)
 				PlaySound("RaidWarning", "Master")
-			elseif spellId == 219958 and destGUID == UnitGUID("player") then -- Mark of Baldrazar
-				 -- 20 sec debuff, explosion on damage taken
+			elseif spellId == 219958 and destGUID == myID then -- Mark of Baldrazar
+				-- 20 sec debuff, explosion on damage taken
 				local msg = "|T".. GetSpellTexture(spellId) ..texString.. spellName .."(ON YOU, AVOID TAKING DAMAGE)"
+				print("|cFF33FF99LegionInvasionTimer|r:", msg)
+				RaidNotice_AddMessage(RaidBossEmoteFrame, msg, colorTbl, 4)
+				PlaySound("RaidWarning", "Master")
+			elseif (spellId == 219367 or spellId == 207576) and destGUID == myID then -- Rain of Fire / Fel Fire
+				local msg = "|T".. GetSpellTexture(spellId) ..texString.. spellName .."(ON YOU, GET OUT)"
 				print("|cFF33FF99LegionInvasionTimer|r:", msg)
 				RaidNotice_AddMessage(RaidBossEmoteFrame, msg, colorTbl, 4)
 				PlaySound("RaidWarning", "Master")
