5c6a703892279e6ffb034c0fe2c545769b1062d7
funkydude <funkydude@users.noreply.github.com>
2016-08-12 22:26:11 +0100

    Play the Raid Warning sound when showing a boss message and tweak the messages a little.

--- Bosses.lua
+++ Bosses.lua
@@ -36,13 +36,13 @@ do
 	local text = {
 		SPELL_CAST_START = {
 			[219112] = "|T".. GetSpellTexture(219112) ..texString.. GetSpellInfo(219112).. " (RUN TO BOSS)", -- Eye of Darkness 45s
-			[219960] = "|T".. GetSpellTexture(219960) ..texString.. GetSpellInfo(219960).. " (FRONTAL CONE DAMAGE)", -- Breath of Shadows 32s
-			[219957] = "|T".. GetSpellTexture(219957) ..texString.. GetSpellInfo(219957).. " (DEBUFF INCOMING)", -- Mark of Baldrazar 46s
-			[217093] = "|T".. GetSpellTexture(217093) ..texString.. GetSpellInfo(217093).. " (AoE MC INCOMING)", -- Shadow Madness 48s
-			[217098] = "|T".. GetSpellTexture(217098) ..texString.. GetSpellInfo(217098).. " (FRONTAL CONE DAMAGE)", -- Carrion Swarm 16s
+			[219960] = "|T".. GetSpellTexture(219960) ..texString.. GetSpellInfo(219960).. " (FRONTAL CONE DMG)", -- Breath of Shadows 32s
+			[219957] = "|T".. GetSpellTexture(219957) ..texString.. GetSpellInfo(219957).. " (DEBUFF INC)", -- Mark of Baldrazar 46s
+			[217093] = "|T".. GetSpellTexture(217093) ..texString.. GetSpellInfo(217093).. " (MIND CONTROL INC)", -- Shadow Madness 48s
+			[217098] = "|T".. GetSpellTexture(217098) ..texString.. GetSpellInfo(217098).. " (FRONTAL CONE DMG)", -- Carrion Swarm 16s
 			[217134] = "|T".. GetSpellTexture(217134) ..texString.. GetSpellInfo(217134).. " (FRONTAL CLEAVE)", -- Vampiric Cleave 33s
 			[217040] = "|T".. GetSpellTexture(217040) ..texString.. GetSpellInfo(217040).. " (THREAT WIPE - SPAWN ADD)", -- Shadow Illusion 35s
-			[216916] = "|T".. GetSpellTexture(216916) ..texString.. GetSpellInfo(216916).. " (FRONTAL CONE DAMAGE)", -- Waves of Dread 35s
+			[216916] = "|T".. GetSpellTexture(216916) ..texString.. GetSpellInfo(216916).. " (FRONTAL CONE DMG)", -- Waves of Dread 35s
 		},
 		SPELL_AURA_REMOVED = {
 			[219112] = "|T".. GetSpellTexture(219112) ..texString.. GetSpellInfo(219112).. " (FINISHED)", -- Eye of Darkness 45s
@@ -52,7 +52,8 @@ do
 		local msg = text[event] and text[event][spellId]
 		if msg then
 			print("|cFF33FF99LegionInvasionTimer|r:", msg)
-			RaidNotice_AddMessage(RaidBossEmoteFrame, msg, colorTbl, 3)
+			RaidNotice_AddMessage(RaidBossEmoteFrame, msg, colorTbl, 4)
+			PlaySound("RaidWarning", "Master")
 		end
 
 		if event == "SPELL_AURA_APPLIED" then
@@ -60,12 +61,14 @@ do
 				 -- 10 sec debuff on tank
 				local msg = "|T".. GetSpellTexture(spellId) ..texString.. spellName.. ": ".. gsub(destName, "%-.+", "*") .."(TANK SWAP)"
 				print("|cFF33FF99LegionInvasionTimer|r:", msg)
-				RaidNotice_AddMessage(RaidBossEmoteFrame, msg, colorTbl, 3)
+				RaidNotice_AddMessage(RaidBossEmoteFrame, msg, colorTbl, 4)
+				PlaySound("RaidWarning", "Master")
 			elseif spellId == 219958 and destGUID == UnitGUID("player") then -- Mark of Baldrazar
 				 -- 20 sec debuff, explosion on damage taken
 				local msg = "|T".. GetSpellTexture(spellId) ..texString.. spellName .."(ON YOU, AVOID TAKING DAMAGE)"
 				print("|cFF33FF99LegionInvasionTimer|r:", msg)
-				RaidNotice_AddMessage(RaidBossEmoteFrame, msg, colorTbl, 3)
+				RaidNotice_AddMessage(RaidBossEmoteFrame, msg, colorTbl, 4)
+				PlaySound("RaidWarning", "Master")
 			end
 		end
 	end
