933adc2607576408c21fe8624a4addfcc2131888
Kris <cblair91@gmail.com>
2016-08-16 20:39:26 +0100

    Optimize system & Overhaul code ;)
    - Demon invasions can increase up to 6 towards the later stages (aka all running), so setup frame.bar to be a table to contain all them, even ones not setup yet (just leave them unused)
    - Instead of stopping the bar (and deleting it in the process), simply just reuse the bar by updating its duration (and re-starting to resetup all the data)
    - Optimize bar loading into a function, and loop (save coding it manually every time)
    - Use a select function for scenarioType, save many underscores
    - For LibCandyBar_Stop, it'll now only be called when the timer expires, therefor a new invasion has begun
    - timeLeftMinutes now no longer reports more than the max time (Blizzard fixed)
    - Don't bother checking for justLoggedIn, as if the event is new, alert them either way
    - Optimize the f.db and f.header values (don't create a local, just init them directly)
    - No longer have a paused variable for startTimer, just detect if achievement ID is nil (also use nil rather than 0, quicker checking)

--- LegionInvasionTimer.lua
+++ LegionInvasionTimer.lua
@@ -1,4 +1,3 @@
-
 local name, mod = ...
 local L = mod.L
 local candy = LibStub("LibCandyBar-3.0")
@@ -17,61 +16,43 @@ frame:RegisterForDrag("LeftButton")
 frame:SetClampedToScreen(true)
 frame:Hide()
 frame:RegisterEvent("PLAYER_LOGIN")
+frame.bar = {}
 
-local function startBar(text, timeLeft, rewardQuestID, icon, count, pause)
-	local bar
+local function startBar(bar, text, timeLeft, rewardQuestID, icon)
+	bar:SetLabel(text:match("[^%:]+:(.+)") or text)
+	bar:SetDuration(timeLeft)
+	if legionTimerDB.icon then
+		bar:SetIcon(icon)
+	end
+	bar:SetTimeVisiblity(true)
+	if rewardQuestID then -- Zone bars
+		if IsQuestFlaggedCompleted(rewardQuestID) then
+			bar:SetColor(unpack(legionTimerDB.colorComplete))
+		else
+			bar:SetColor(unpack(legionTimerDB.colorIncomplete))
+		end
+	else -- Searching bars
+		bar:SetTimeVisibility(false)
+	end
+	bar:Start(7200) -- 2hrs = 60*2 = 120min = 120*60 = 7,200sec
+end
+
+local function setupBar(bar, count, icon)
 	if count == 1 then
-		if frame.bar1 then frame.bar1:Stop(true) end
-		frame.bar1 = candy:New(media:Fetch("statusbar", legionTimerDB.barTexture), legionTimerDB.width, legionTimerDB.height)
-		bar = frame.bar1
 		if legionTimerDB.growUp then
 			bar:SetPoint("BOTTOM", name, "TOP")
 		else
 			bar:SetPoint("TOP", name, "BOTTOM")
 		end
-	elseif count == 2 then
-		if frame.bar2 then frame.bar2:Stop() end
-		frame.bar2 = candy:New(media:Fetch("statusbar", legionTimerDB.barTexture), legionTimerDB.width, legionTimerDB.height)
-		bar = frame.bar2
-		if legionTimerDB.growUp then
-			frame.bar2:SetPoint("BOTTOMLEFT", frame.bar1, "TOPLEFT", 0, legionTimerDB.spacing)
-			frame.bar2:SetPoint("BOTTOMRIGHT", frame.bar1, "TOPRIGHT", 0, legionTimerDB.spacing)
-		else
-			frame.bar2:SetPoint("TOPLEFT", frame.bar1, "BOTTOMLEFT", 0, -legionTimerDB.spacing)
-			frame.bar2:SetPoint("TOPRIGHT", frame.bar1, "BOTTOMRIGHT", 0, -legionTimerDB.spacing)
-		end
 	else
-		if frame.bar3 then frame.bar3:Stop() end
-		frame.bar3 = candy:New(media:Fetch("statusbar", legionTimerDB.barTexture), legionTimerDB.width, legionTimerDB.height)
-		bar = frame.bar3
 		if legionTimerDB.growUp then
-			frame.bar3:SetPoint("BOTTOMLEFT", frame.bar2, "TOPLEFT", 0, legionTimerDB.spacing)
-			frame.bar3:SetPoint("BOTTOMRIGHT", frame.bar2, "TOPRIGHT", 0, legionTimerDB.spacing)
+			bar:SetPoint("BOTTOMLEFT", frame.bar[count - 1], "TOPLEFT", 0, legionTimerDB.spacing)
+			bar:SetPoint("BOTTOMRIGHT", frame.bar[count - 1], "TOPRIGHT", 0, legionTimerDB.spacing)
 		else
-			frame.bar3:SetPoint("TOPLEFT", frame.bar2, "BOTTOMLEFT", 0, -legionTimerDB.spacing)
-			frame.bar3:SetPoint("TOPRIGHT", frame.bar2, "BOTTOMRIGHT", 0, -legionTimerDB.spacing)
+			bar:SetPoint("TOPLEFT", frame.bar[count - 1], "BOTTOMLEFT", 0, -legionTimerDB.spacing)
+			bar:SetPoint("TOPRIGHT", frame.bar[count - 1], "BOTTOMRIGHT", 0, -legionTimerDB.spacing)
 		end
 	end
-
-	bar:SetLabel(text:match("[^%:]+:(.+)") or text)
-	bar.candyBarLabel:SetJustifyH(legionTimerDB.alignZone)
-	bar.candyBarDuration:SetJustifyH(legionTimerDB.alignTime)
-	bar:SetDuration(timeLeft)
-	if rewardQuestID > 0 then
-		if IsQuestFlaggedCompleted(rewardQuestID) then
-			bar:SetColor(unpack(legionTimerDB.colorComplete))
-			bar:Set("LegionInvasionTimer:complete", 1)
-		else
-			bar:SetColor(unpack(legionTimerDB.colorIncomplete))
-			bar:Set("LegionInvasionTimer:complete", 0)
-		end
-	end
-	bar:SetTextColor(unpack(legionTimerDB.colorText))
-	if legionTimerDB.icon then
-		bar:SetIcon(icon)
-	end
-	bar:SetTimeVisibility(legionTimerDB.timeText)
-	bar:SetFill(legionTimerDB.fill)
 	local flags = nil
 	if legionTimerDB.monochrome and legionTimerDB.outline ~= "NONE" then
 		flags = "MONOCHROME," .. legionTimerDB.outline
@@ -82,18 +63,14 @@ local function startBar(text, timeLeft, rewardQuestID, icon, count, pause)
 	end
 	bar.candyBarLabel:SetFont(media:Fetch("font", legionTimerDB.font), legionTimerDB.fontSize, flags)
 	bar.candyBarDuration:SetFont(media:Fetch("font", legionTimerDB.font), legionTimerDB.fontSize, flags)
-	if pause then -- Searching bars
-		bar:Start()
-		bar:Pause()
-		bar:SetTimeVisibility(false)
-	elseif rewardQuestID > 0 then -- Zone bars
-		bar:Start(7200) -- 2hrs = 60*2 = 120min = 120*60 = 7,200sec
-	else
-		bar:Start() -- Boss bars
-	end
+	bar.candyBarLabel:SetJustifyH(legionTimerDB.alignZone)
+	bar.candyBarDuration:SetJustifyH(legionTimerDB.alignTime)
+	bar:SetTextColor(unpack(legionTimerDB.colorText))
+	bar:SetTimeVisibility(legionTimerDB.timeText)
+	bar:SetFill(legionTimerDB.fill)
+	startBar(frame.bar[i], "Searching...", 7200, nil, 132177) -- 132177 = Interface\\Icons\\Ability_Hunter_MasterMarksman
 end
 
-local hasPausedBars, justLoggedIn = false, true
 local function findTimer()
 	-- 3 Legion Invasion: Northern Barrens 0 43282
 	-- 4 Legion Invasion: Westfall 0 43245
@@ -102,31 +79,22 @@ local function findTimer()
 	-- 7 Legion Invasion: Hillsbrad 0 43285
 	-- 8 Legion Invasion: Azshara 0 43301
 
-	local first = true
 	local count = 1
 	for i = 3, 8 do
 		local zone, timeLeftMinutes, rewardQuestID = GetInvasionInfo(i)
-		if timeLeftMinutes and timeLeftMinutes > 1 and timeLeftMinutes < 121 then -- On some realms timeLeftMinutes can return massive values during the initialization of a new event
-			startBar(zone, timeLeftMinutes * 60, rewardQuestID, 236292, count) -- 236292 = Interface\\Icons\\Ability_Warlock_DemonicEmpowerment
-			if count == 3 then break end -- 3 events
+		if timeLeftMinutes and timeLeftMinutes > 0 then
+			startBar(frame.bar[count], zone, timeLeftMinutes * 60, rewardQuestID, 236292) -- 236292 = Interface\\Icons\\Ability_Warlock_DemonicEmpowerment
 			count = count + 1
-			if hasPausedBars and not justLoggedIn and timeLeftMinutes > 110 then
-				hasPausedBars = false
-				Timer(30, findTimer) -- Sometimes Blizz doesn't reset the quest ID very quickly, do another check to fix colors if so
+			if timeLeftMinutes > 110 then
 				FlashClientIcon()
 				print("|cFF33FF99LegionInvasionTimer|r:", L.invasionsAvailable)
 				RaidNotice_AddMessage(RaidBossEmoteFrame, L.invasionsAvailable, mod.c)
 				PlaySound("RaidWarning", "Master")
 			end
-			justLoggedIn = false
 		end
 	end
 
 	if count == 1 then
-		if not hasPausedBars then
-			hasPausedBars = true
-			startBar("Searching...", 7200, 0, 132177, count, true) -- 132177 = Interface\\Icons\\Ability_Hunter_MasterMarksman
-		end
 		Timer(3, findTimer) -- Start hunting for the next event
 	end
 end
@@ -167,6 +135,11 @@ frame:SetScript("OnEvent", function(f)
 		legionTimerDB.texture = nil
 	end
 
+	for i = 1, 6 do
+		frame.bar[i] = candy:New(media:Fetch("statusbar", legionTimerDB.barTexture), legionTimerDB.width, legionTimerDB.height)
+		setupBar(frame.bar[i], i)
+	end
+
 	f:Show()
 	f:SetScript("OnDragStart", function(f) f:StartMoving() end)
 	f:SetScript("OnDragStop", function(f) f:StopMovingOrSizing() end)
@@ -186,16 +159,12 @@ frame:SetScript("OnEvent", function(f)
 		GameTooltip:Show()
 	end)
 	f:SetScript("OnLeave", GameTooltip_Hide)
-	local bg = f:CreateTexture(nil, "PARENT")
-	bg:SetAllPoints(f)
-	bg:SetColorTexture(0, 1, 0, 0.3)
-	f.bg = bg
-	local header = f:CreateFontString(nil, "OVERLAY", "TextStatusBarText")
-	header:SetAllPoints(f)
-	header:SetText(name)
-	f.header = header
-	f.startBar = startBar
-	f.db = legionTimerDB
+	f.bg = f:CreateTexture(nil, "PARENT")
+	f.bg:SetAllPoints(f)
+	f.bg:SetColorTexture(0, 1, 0, 0.3)
+	f.header = f:CreateFontString(nil, "OVERLAY", "TextStatusBarText")
+	f.header:SetAllPoints(f)
+	f.header:SetText(name)
 
 	if legionTimerDB.lock then
 		f:EnableMouse(false)
@@ -203,26 +172,17 @@ frame:SetScript("OnEvent", function(f)
 		f.header:Hide()
 	end
 
-	candy.RegisterCallback(name, "LibCandyBar_Stop", function(_, bar, dontScan)
-		if not dontScan and bar == frame.bar1 and bar:Get("LegionInvasionTimer:complete") then
+	candy.RegisterCallback(name, "LibCandyBar_Stop", function(_ bar)
+		if not dontScan and bar == frame.bar[1] then
 			Timer(2, findTimer) -- Event over, start hunting for the next event
-		end
-		if bar == frame.bar1 then
-			frame.bar1 = nil
-		elseif bar == frame.bar2 then
-			frame.bar2 = nil
-		elseif bar == frame.bar3 then
-			frame.bar3 = nil
 		end
 	end)
 
 	findTimer()
 	f:RegisterEvent("SCENARIO_COMPLETED")
 	f:SetScript("OnEvent", function()
-		local _,_,_,_,_,_,_,_,_,scenarioType = C_Scenario.GetInfo()
-		if scenarioType == 4 then -- LE_SCENARIO_TYPE_LEGION_INVASION = 4
+		if select(10, C_Scenario.GetInfo()) == 4 then -- LE_SCENARIO_TYPE_LEGION_INVASION = 4
 			Timer(8, findTimer) -- Update bar color
 		end
 	end)
 end)
-
