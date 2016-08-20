904f27c9bc5cbf2cbea1f3186c1aa59f4e5e9f9b
funkydude <funkydude@users.noreply.github.com>
2016-08-20 21:06:47 +0100

    Mousing over a bar will now show a tooltip with the completion status of the achievement on that character.

--- LegionInvasionTimer.lua
+++ LegionInvasionTimer.lua
@@ -54,6 +54,29 @@ local function startBar(text, timeLeft, rewardQuestID, icon, count, pause)
 		end
 	end
 
+	bar:SetScript("OnEnter", function(f)
+		GameTooltip:SetOwner(f, "ANCHOR_NONE")
+		GameTooltip:SetPoint("BOTTOM", f, "TOP")
+		local id = 11201 -- Defender of Azeroth: Legion Invasions
+		local _, name, _, _, month, day, year, description, _, _, _, _, wasEarnedByMe = GetAchievementInfo(id)
+		if wasEarnedByMe then
+			GameTooltip:AddDoubleLine(name, _G.SHORTDATE:format(day, month, year), nil, nil, nil, .5, .5, .5)
+		else
+			GameTooltip:AddLine(name, nil, nil, nil, .5, .5, .5)
+		end
+		GameTooltip:AddLine(description, 1, 1, 1, true)
+		for i = 1, GetAchievementNumCriteria(id) do
+			local criteriaString, criteriaType, completed = GetAchievementCriteriaInfo(id, i)
+			if completed == false then
+				criteriaString = "|CFF808080 - " .. criteriaString .. "|r"
+			else
+				criteriaString = "|CFF00FF00 - " .. criteriaString .. "|r"
+			end
+			GameTooltip:AddLine(criteriaString)
+		end
+		GameTooltip:Show()
+	end)
+	bar:SetScript("OnLeave", GameTooltip_Hide)
 	bar:SetParent(frame)
 	bar:SetLabel(text:match("[^%:]+: ?(.+)") or text) -- Strip out the "Legion Invasion: " part and leave the zone name behind.
 	bar.candyBarLabel:SetJustifyH(legionTimerDB.alignZone)
