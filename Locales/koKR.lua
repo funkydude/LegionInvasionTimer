36f3b3f33c8a11496140f831b76e99777baef3b1
WetU <yuk6196@gmail.com>
2017-04-30 01:53:56 +0900

    Update koKR.lua (#46)

--- Locales/koKR.lua
+++ Locales/koKR.lua
@@ -3,8 +3,8 @@ if GetLocale() ~= "koKR" then return end
 local _, mod = ...
 local L = mod.L
 
---L.firstRunWarning = "Timers will not be shown until you see your first invasion."
---L.underAttack = "|T236292:15:15:0:0:64:64:4:60:4:60|t %s is under attack!"
---L.tooltipClick = "|cffeda55fClick|r to drag and move."
---L.tooltipClickOptions = "|cffeda55fRight-Click|r to open options."
---L.nextInvasion = "Next Invasion"
+L.firstRunWarning = "타이머는 첫번째 침공을 확인하기 전까지 표시되지 않습니다."
+L.underAttack = "|T236292:15:15:0:0:64:64:4:60:4:60|t %s|1이;가; 공격받고 있습니다!"
+L.tooltipClick = "끌어서 이동하려면 |cffeda55f클릭|r하세요."
+L.tooltipClickOptions = "옵션을 열려면 |cffeda55f오른쪽-클릭|r하세요."
+L.nextInvasion = "다음 침공"
