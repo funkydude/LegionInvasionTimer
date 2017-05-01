8ba26d2d3a7b17d051d2086601f36152e9d79543
bullsei <bullsei@users.noreply.github.com>
2017-05-01 16:44:08 +0200

    Update deDE.lua

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -3,10 +3,10 @@ if GetLocale() ~= "deDE" then return end
 local _, mod = ...
 local L = mod.L
 
---L.firstRunWarning = "Timers will not be shown until you see your first invasion."
---L.underAttack = "|T236292:15:15:0:0:64:64:4:60:4:60|t %s is under attack!"
-L.tooltipClick = "|cffeda55fKlick|r zum ziehen und bewegen."
-L.tooltipClickOptions = "|cffeda55fRechts-Klick|r um Optionen zu öffnen."
---L.nextInvasions = "Next Invasions"
+L.firstRunWarning = "Timer werden erst angezeigt, sobald du die erste Invasion siehst."
+L.underAttack = "|T236292:15:15:0:0:64:64:4:60:4:60|t %s wird angegriffen!"
+L.tooltipClick = "|cffeda55fKlick|r zum Verschieben."
+L.tooltipClickOptions = "|cffeda55fRechtsklick|r, um Optionen zu öffnen."
+L.nextInvasions = "Nächsten Invasionen"
 --L.next = "Next"
---L.waiting = "Waiting"
+L.waiting = "Warten"
