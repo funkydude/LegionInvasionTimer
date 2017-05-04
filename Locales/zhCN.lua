7488c5945f57676a5507be78ae63b9d7ea00a75f
Adavak LEE <adavak@adavak.com>
2017-05-04 12:58:05 +0800

    zhCN update. (#57)

--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -3,10 +3,10 @@ if GetLocale() ~= "zhCN" then return end
 local _, mod = ...
 local L = mod.L
 
---L.firstRunWarning = "Timers will not be shown until you see your first invasion."
---L.underAttack = "|T236292:15:15:0:0:64:64:4:60:4:60|t %s is under attack!"
+L.firstRunWarning = "遇到第一次入侵之前不会显示计时器。"
+L.underAttack = "|T236292:15:15:0:0:64:64:4:60:4:60|t %s 被入侵！"
 L.tooltipClick = "|cffeda55f点击|r 拖拽并移动。"
 L.tooltipClickOptions = "|cffeda55f右击|r 打开选项。"
---L.nextInvasions = "Next Invasions"
---L.next = "Next"
---L.waiting = "Waiting"
+L.nextInvasions = "下次入侵"
+L.next = "下次"
+L.waiting = "等待中"
