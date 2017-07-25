b02a015b353424f7d9462a44c4a4e11a7ca4b952
EK <eke00372@gmail.com>
2017-07-26 07:17:07 +0800

    Update zhTW.lua

--- Locales/zhTW.lua
+++ Locales/zhTW.lua
@@ -3,10 +3,11 @@ if GetLocale() ~= "zhTW" then return end
 local _, mod = ...
 local L = mod.L
 
---L.firstRunWarning = "Timers will not be shown until you see your first invasion."
---L.underAttack = "|T236292:15:15:0:0:64:64:4:60:4:60|t %s is under attack!"
-L.tooltipClick = "|cffeda55f左鍵點擊|r 拖曳與移動。"
-L.tooltipClickOptions = "|cffeda55f右鍵點擊|r 開啟選項。"
---L.nextInvasions = "Next Invasions"
---L.next = "Next"
---L.waiting = "Waiting"
+L.firstRunWarning = "計時器將在第一次遇到入侵之後顯示。"
+L.underAttack = "|T236292:15:15:0:0:64:64:4:60:4:60|t %s被入侵！"
+L.tooltipClick = "|cffeda55f左鍵點擊|r拖曳與移動。"
+L.tooltipClickOptions = "|cffeda55f右鍵點擊|r開啟選項。"
+L.nextInvasions = "下次入侵"
+L.next = "下次"
+L.waiting = "等待中"
+
