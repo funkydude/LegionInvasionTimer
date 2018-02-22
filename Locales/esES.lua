6aa475c79966e243c8130e944a4fcf6f8471f676
funkydude <funkydude@users.noreply.github.com>
2018-02-22 22:30:53 +0000

    Remove comments

--- Locales/esES.lua
+++ Locales/esES.lua
@@ -3,10 +3,10 @@ if GetLocale() ~= "esES" and GetLocale() ~= "esMX" then return end
 local _, mod = ...
 local L = mod.L
 
---L.firstRunWarning = "Los tiempos no se mostrarán hasta que veas tu primera invasión"
---L.underAttack = "¡Están atacando |T236292:15:15:0:0:64:64:4:60:4:60|t %s!"
+L.firstRunWarning = "Los tiempos no se mostrarán hasta que veas tu primera invasión"
+L.underAttack = "¡Están atacando |T236292:15:15:0:0:64:64:4:60:4:60|t %s!"
 L.tooltipClick = "|cffeda55fClick|r para arrastrar y mover."
 L.tooltipClickOptions = "|cffeda55fClick derecho|r para abrir las opciones."
---L.nextInvasions = "Siguientes Invasiones"
---L.next = "Siguiente"
---L.waiting = "Esperando"
+L.nextInvasions = "Siguientes Invasiones"
+L.next = "Siguiente"
+L.waiting = "Esperando"
