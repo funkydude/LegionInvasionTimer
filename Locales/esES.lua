b69aa392ea025767dbe1dee59999fa969f4c2ab6
funkydude <funkydude@users.noreply.github.com>
2019-02-14 23:14:42 +0000

    locale tweak

--- Locales/esES.lua
+++ Locales/esES.lua
@@ -4,7 +4,7 @@ local _, mod = ...
 local L = mod.L
 
 L.firstRunWarning = "Los tiempos no se mostrarán hasta que veas tu primera invasión"
-L.underAttack = "¡Están atacando |T236292:15:15:0:0:64:64:4:60:4:60|t %s!"
+L.underAttack = "|T236292:15:15:0:0:64:64:4:60:4:60|t ¡Están atacando %s!"
 L.nextInvasions = "Siguientes Invasiones"
 L.next = "Siguiente"
 L.waiting = "Esperando"
