dfe7977b1bf8a5be39490858f96c3f4f3243a1f9
funkydude <funkydude@users.noreply.github.com>
2019-02-14 23:04:42 +0000

    Update ruRU

--- Locales/ruRU.lua
+++ Locales/ruRU.lua
@@ -3,8 +3,8 @@ if GetLocale() ~= "ruRU" then return end
 local _, mod = ...
 local L = mod.L
 
---L.firstRunWarning = "Timers will not be shown until you see your first invasion."
---L.underAttack = "|T236292:15:15:0:0:64:64:4:60:4:60|t %s is under attack!"
---L.nextInvasions = "Next Invasions"
---L.next = "Next"
---L.waiting = "Waiting"
+L.firstRunWarning = "Таймеры не будут показаны, пока вы не увидите свое первое вторжение."
+L.underAttack = "|T236292:15:15:0:0:64:64:4:60:4:60|t %s находится под атакой!"
+L.nextInvasions = "Следующее вторжение"
+L.next = "Следующее"
+L.waiting = "Ожидание"
