2c909239b72163960f3f3c308e194801010e90e0
funkydude <funkydude@users.noreply.github.com>
2019-03-13 03:45:04 +0000

    Update frFR & itIT.

--- Locales/frFR.lua
+++ Locales/frFR.lua
@@ -3,8 +3,8 @@ if GetLocale() ~= "frFR" then return end
 local _, mod = ...
 local L = mod.L
 
---L.firstRunWarning = "Timers will not be shown until you see your first invasion."
---L.underAttack = "|T236292:15:15:0:0:64:64:4:60:4:60|t %s is under attack!"
---L.nextInvasions = "Next Invasions"
---L.next = "Next"
---L.waiting = "Waiting"
+L.firstRunWarning = "Le timer n'apparaitra pas avant la première invasion."
+L.underAttack = "|T236292:15:15:0:0:64:64:4:60:4:60|t %s est envahie!"
+L.nextInvasions = "Prochaines invasions"
+L.next = "Suivante"
+L.waiting = "En Attente"
--- Locales/itIT.lua
+++ Locales/itIT.lua
@@ -3,8 +3,8 @@ if GetLocale() ~= "itIT" then return end
 local _, mod = ...
 local L = mod.L
 
---L.firstRunWarning = "Timers will not be shown until you see your first invasion."
---L.underAttack = "|T236292:15:15:0:0:64:64:4:60:4:60|t %s is under attack!"
---L.nextInvasions = "Next Invasions"
---L.next = "Next"
---L.waiting = "Waiting"
+L.firstRunWarning = "I timer non verranno visualizzati fino a quando non vedrai la tua prima invasione."
+L.underAttack = "|T236292:15:15:0:0:64:64:4:60:4:60|t %s è sotto attacco!"
+L.nextInvasions = "Prossime invasioni"
+L.next = "Prossima"
+L.waiting = "In attesa"
