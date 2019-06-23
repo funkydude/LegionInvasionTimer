fcb7fc61c0e2945b5087df1fc4d32c77f08006f2
funkydude <funkydude@users.noreply.github.com>
2019-06-23 17:00:31 +0100

    Update ptBR

--- Locales/ptBR.lua
+++ Locales/ptBR.lua
@@ -3,8 +3,8 @@ if GetLocale() ~= "ptBR" then return end
 local _, mod = ...
 local L = mod.L
 
---L.firstRunWarning = "Timers will not be shown until you see your first invasion."
---L.underAttack = "|T236292:15:15:0:0:64:64:4:60:4:60|t %s is under attack!"
---L.nextInvasions = "Next Invasions"
---L.next = "Next"
---L.waiting = "Waiting"
+L.firstRunWarning = "O cronômetro não iniciará até que você veja sua primeira invasão."
+L.underAttack = "|T236292:15:15:0:0:64:64:4:60:4:60|t Estão atacando %s!"
+L.nextInvasions = "Próximas Invasões"
+L.next = "Próxima"
+L.waiting = "Esperando"
